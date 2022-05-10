3a93872969d0f49367abbed03c17efdf8a339cc5
Ha Thu <tranhathu6989@gmail.com>
2022-05-10 16:56:14 +0200

    condition rending option 1

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,18 @@
 function Movie(props) {
+  let content;
+  if (props.rating > 8) {
+    content = <span>RECOMMENDED</span>;
+  } else {
+    content = "";
+  }
+
   return (
     <div>
       <h3>{props.title}</h3>
       <p>
         Year: {props.year} <br />
         Rating: {props.rating} <br />
-        <span>RECOMMENDED</span>
+        {content}
       </p>
     </div>
   );
